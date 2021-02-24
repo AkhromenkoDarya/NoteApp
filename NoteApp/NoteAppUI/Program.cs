af13226625794e681d061312c653d936f35b3898
AkhromenkoDarya <otulissa2012@yandex.ru>
2021-02-24 14:33:02 +0300

    Добавлен новый комментарий

--- NoteApp/NoteAppUI/Program.cs
+++ NoteApp/NoteAppUI/Program.cs
@@ -15,6 +15,7 @@ namespace NoteAppUI
         [STAThread]
         static void Main()
         {
+            // Новый комментарий
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
