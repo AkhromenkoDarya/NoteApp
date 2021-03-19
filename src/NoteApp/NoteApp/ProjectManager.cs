754a9cb4b906916c85a00ef97653e4994aa1d711
AkhromenkoDarya <otulissa2012@yandex.ru>
2021-03-19 13:30:14 +0300

    Создан класс "Менеджер проекта" в проекте логики приложения

---
+++ src/NoteApp/NoteApp/ProjectManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace NoteApp
+{
+    public static class ProjectManager
+    {
+        private const string Filename = @"..\My Documents\NoteApp.notes";
+
+        public static void SaveToFile(Project data)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+
+            using (StreamWriter sw = new StreamWriter(Filename))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, data);
+            }
+        }
+
+        public static Project LoadFromFile()
+        {
+            var project = new Project();
+
+            JsonSerializer serializer = new JsonSerializer();
+
+            using (StreamReader sr = new StreamReader(Filename))
+            using (JsonReader reader = new JsonTextReader(sr))
+            {
+                project = serializer.Deserialize<Project>(reader);
+            }
+
+            return project;
+        }
+    }
+}
