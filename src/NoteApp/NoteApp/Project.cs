09af59e07414738c050f05ed8419f054f0a53bf9
AkhromenkoDarya <otulissa2012@yandex.ru>
2021-03-19 12:11:26 +0300

    Создан класс "Проект" в проекте логики приложения

---
+++ src/NoteApp/NoteApp/Project.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp
+{
+    public class Project
+    {
+        List<Note> _notes = null;
+
+        public List<Note> Notes
+        {
+            get 
+            {
+                return _notes;
+            }
+
+            private set
+            {
+                _notes = value;
+            }
+        }
+
+        public Project()
+        {
+            Notes = new List<Note>();
+        }
+    }
+}
