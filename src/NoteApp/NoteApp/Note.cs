0679441e1ba7a4a39cb41f725f0abe728f9adf9f
AkhromenkoDarya <otulissa2012@yandex.ru>
2021-03-26 10:03:21 +0300

    Изменен класс "Заметка:  исправлены названия свойств, отредактированы комментарии

--- src/NoteApp/NoteApp/Note.cs
+++ src/NoteApp/NoteApp/Note.cs
@@ -9,15 +9,15 @@ namespace NoteApp
 {
     /// <summary>
     /// <para>Класс "Заметка".</para>
-    /// <para>Хранит информацию о названии, категории, тексте, дате и времени создания,
-    /// а также дате и времени последнего изменения текстовой заметки пользователя.</para>
+    /// <para>Хранит информацию о названии, категории, тексте, времени создания и времени 
+    /// последнего изменения текстовой заметки пользователя.</para>
     /// <para>Реализует интерфейс <see cref="ICloneable"/>.</para>
     /// </summary>
     public class Note : ICloneable
     {
         /// <summary>
         /// <para>Название заметки.</para>
-        /// <para>Ограничено 50-ю символами. Значение по умолчанию - "Без названия".</para>
+        /// <para>Ограничено 50 символами. Значение по умолчанию - "Без названия".</para>
         /// </summary>
         private string _title;
 
@@ -34,9 +34,8 @@ namespace NoteApp
         /// <summary>
         /// Возвращает и задает название заметки.
         /// </summary>
-        /// <exception cref="ArgumentException">Возникает при попытке присвоения 
-        /// переменной <see cref="_title"/> значения null или в случае, когда название 
-        /// заметки превышает 50 символов.</exception>
+        /// <exception cref="ArgumentException">Возникает, если название заметки содержит более 
+        /// 50 символов.</exception>
         public string Title
         {
             get
@@ -46,22 +45,20 @@ namespace NoteApp
 
             set
             {
-                if (value == null)
+                // Если название заметки содержит больше 50 символов, то...
+                if (value.Length > 50)
                 {
-                    throw new ArgumentException("Попытка присваивания переменной _title " +
-                        "значения null.");
-                }
-
-                // Название заметки ограничено 50 символами.
-                else if (value.Length > 50)
-                {
-                    throw new ArgumentException("Название заметки не должно превышать " +
-                        "50 символов.");
+                    // ...бросаем исключение, сообщающее, что аргумент, передаваемый свойству,
+                    // является недопустимым.
+                    throw new ArgumentException("Название должно содержать не более 50 " +
+                        "символов.");
                 }
                 else
                 {
                     _title = value;
-                    UpdateLastChangeDateTime();
+
+                    // Обновляем время последнего изменения заметки при изменении ее названия.
+                    ModifyTime();
                 }
             }
         }
@@ -79,15 +76,15 @@ namespace NoteApp
             set
             {
                 _category = value;
-                UpdateLastChangeDateTime();
+
+                // Обновляем время последнего изменения заметки при изменении ее категории.
+                ModifyTime();
             }
         }
 
         /// <summary>
         /// Возвращает и задает текст заметки.
         /// </summary>
-        /// <exception cref="ArgumentException">Возникает при попытке присвоения 
-        /// переменной <see cref="_text"/> значения null.</exception>
         public string Text
         {
             get
@@ -97,28 +94,22 @@ namespace NoteApp
 
             set
             {
-                if (value == null)
-                {
-                    throw new ArgumentException("Попытка присваивания переменной _text " +
-                        "значения null.");
-                }
-                else
-                {
-                    _text = value;
-                    UpdateLastChangeDateTime();
-                }
+                _text = value;
+
+                // Обновляем время последнего изменения заметки при изменении ее текста.
+                ModifyTime();
             }
         }
 
         /// <summary>
-        /// Возвращает и задает дату и время создания заметки.
+        /// Возвращает и задает время создания заметки.
         /// </summary>
-        public DateTime CreationDateTime { get; private set; }
+        public DateTime CreationTime { get; private set; }
 
         /// <summary>
-        /// Возвращает и задает дату и время последнего изменения заметки.
+        /// Возвращает и задает время последнего изменения заметки.
         /// </summary>
-        public DateTime LastChangeDateTime { get; private set; }
+        public DateTime ModificationTime { get; private set; }
 
         /// <summary>
         /// Создает экземпляр класса <see cref="Note"/>.
@@ -128,11 +119,9 @@ namespace NoteApp
         /// "Категория заметки":</para> 
         /// <para>"Работа", "Дом", "Здоровье и Спорт", "Люди", "Документы", "Финансы",
         /// "Разное".</para></param>
-        /// <param name="text"><para>Текст заметки.</para>  
-        /// <para>Не может иметь значение null.</para></param>
-        /// <param name="title"><para>Название заметки.</para> 
-        /// <para>Не может иметь значение null или превышать 50 символов.</para></param>
-        public Note(NoteCategory category, string text, string title = "Без названия") : 
+        /// <param name="text">Текст заметки.</param>
+        /// <param name="title">Название заметки. Ограничено 50 символами.</param>
+        public Note(NoteCategory category, string text, string title = "Без названия") :
             this(title, category, text, DateTime.Now, DateTime.Now)
         {
 
@@ -141,26 +130,24 @@ namespace NoteApp
         /// <summary>
         /// Создает экземпляр класса <see cref="Note"/>.
         /// </summary>
-        /// <param name="title"><para>Название заметки.</para> 
-        /// <para>Не может иметь значение null или превышать 50 символов.</para></param>
+        /// <param name="title">Название заметки. Ограничено 50 символами.</param>
         /// <param name="category"><para>Категория заметки.</para> 
         /// <para>Может принимать одно из следующих значений, содержащихся в перечислении 
         /// "Категория заметки":</para> 
         /// <para>"Работа", "Дом", "Здоровье и Спорт", "Люди", "Документы", "Финансы",
         /// "Разное".</para></param>
-        /// <param name="text"><para>Текст заметки.</para>  
-        /// <para>Не может иметь значение null.</para></param>
-        /// <param name="creationDateTime">Дата и время создания заметки.</param>
-        /// <param name="lastChangeDateTime">Дата и время последнего изменения заметки.</param>
+        /// <param name="text">Текст заметки.</param>
+        /// <param name="creationTime">Время создания заметки.</param>
+        /// <param name="modificationTime">Время последнего изменения заметки.</param>
         [JsonConstructor]
-        private Note(string title, NoteCategory category, string text, DateTime creationDateTime,
-            DateTime lastChangeDateTime)
+        private Note(string title, NoteCategory category, string text, DateTime creationTime,
+            DateTime modificationTime)
         {
             Title = title;
             Category = category;
             Text = text;
-            CreationDateTime = creationDateTime;
-            LastChangeDateTime = lastChangeDateTime;
+            CreationTime = creationTime;
+            ModificationTime = modificationTime;
         }
 
         /// <summary>
@@ -174,12 +161,12 @@ namespace NoteApp
         }
 
         /// <summary>
-        /// Метод, обновляющий дату и время последнего изменения заметки при каждом 
-        /// изменении названия, категории или текста заметки.
+        /// Метод, изменяющий время последнего изменения заметки при каждом изменении 
+        /// названия, категории или текста заметки.
         /// </summary>
-        private void UpdateLastChangeDateTime()
+        private void ModifyTime()
         {
-            LastChangeDateTime = DateTime.Now;
+            ModificationTime = DateTime.Now;
         }
     }
 }
